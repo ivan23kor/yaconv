b67880cc77cb5c61446359c9bf53f7807e9e50b7
Ivan Korostelev <ivan23kor@gmail.com>
2021-08-03 20:06:38 +0000

    Adjust width in printTensor
    Automatically adjusts Setw depending on number of digits of the maximum
    number

--- utils.cpp
+++ utils.cpp
@@ -1,16 +1,15 @@
+#include "utils.h"
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
 #include <iostream>
-#include "utils.h"
-
 
 using namespace std;
 
-
 float *allocateTensor(unsigned Size) { return new float[Size]; }
 
-bool tensorsEqual(float *T1, float *T2, const unsigned Size, const float Epsilon) {
+bool tensorsEqual(float *T1, float *T2, const unsigned Size,
+                  const float Epsilon) {
   for (unsigned i = 0; i < Size; ++i)
     if (abs(T1[i] - T2[i]) > Epsilon)
       return false;
@@ -22,24 +21,42 @@ void randomizeTensor(float *&Tensor, unsigned Size) {
   for (unsigned i = 0; i < Size; ++i)
     Tensor[i] = rand() % 255;
 }
- 
+
 void fillSerialTensor(float *&Tensor, unsigned Size) {
   for (unsigned i = 0; i < Size; ++i)
     Tensor[i] = i + 1;
 }
 
-void printVector(float *Vector, unsigned Len, const string Pre="", const string Post="\n") {
+void printVector(float *Vector, unsigned Len, const string Pre = "",
+                 const string Post = "\n", int Setw=3) {
   cout << Pre << "[" << Vector[0];
   for (unsigned i = 1; i < Len; ++i)
-    cout << setw(5) << Vector[i];
+    cout << setw(Setw + 1) << Vector[i];
   cout << "]" << Post;
 }
 
-void printTensor(float *Tensor, vector<unsigned> Sizes, string Pre, const string Post, bool First) {
+void printTensor(float *Tensor, vector<unsigned> Sizes, string Pre,
+                 const string Post, bool First, int Setw) {
+
+  // This block only serves to determine setw width
+  if (Setw == -1) {
+    unsigned Size = 1;
+    for (const auto &s : Sizes)
+      Size *= s;
+
+    float MaxNum = 0.;
+    for (unsigned i = 0; i < Size; ++i)
+      MaxNum = max(MaxNum, Tensor[i]);
+
+    Setw = 1;
+    while ((MaxNum /= 10.) > 1.)
+      ++Setw;
+  }
+
   const string FirstPre = First ? "" : Pre;
   Pre += " ";
   if (Sizes.size() == 1) {
-    printVector(Tensor, Sizes[0], FirstPre, Post);
+    printVector(Tensor, Sizes[0], FirstPre, Post, Setw);
     return;
   }
 
@@ -47,23 +64,23 @@ void printTensor(float *Tensor, vector<unsigned> Sizes, string Pre, const string
 
   vector<unsigned> NewSizes(++Sizes.begin(), Sizes.end());
   if (Sizes[0] == 1) {
-    printTensor(Tensor, NewSizes, Pre, "]" + Post);
+    printTensor(Tensor, NewSizes, Pre, "]" + Post, true, Setw);
     return;
   }
 
-  printTensor(Tensor, NewSizes, Pre, "\n");
+  printTensor(Tensor, NewSizes, Pre, "\n", true, Setw);
 
   const string Newlines = string(NewSizes.size() - 1, '\n');
   cout << Newlines;
 
   unsigned Stride = 1;
-  for (const auto &s: NewSizes)
+  for (const auto &s : NewSizes)
     Stride *= s;
 
   float *End = Tensor + (Sizes[0] - 1) * Stride;
   for (Tensor = Tensor + Stride; Tensor != End; Tensor += Stride) {
-    printTensor(Tensor, NewSizes, Pre, "\n", false);
+    printTensor(Tensor, NewSizes, Pre, "\n", false, Setw);
     cout << Newlines;
   }
-  printTensor(Tensor, NewSizes, Pre, "]" + Post, false);
+  printTensor(Tensor, NewSizes, Pre, "]" + Post, false, Setw);
 }
--- utils.h
+++ utils.h
@@ -1,19 +1,20 @@
-#ifndef __UTILS_H_
-#define __UTILS_H_
-
+#ifndef _UTILS_H_
+#define _UTILS_H_
 
 #include <string>
 #include <vector>
 
-
 float *allocateTensor(unsigned Size);
 
-bool tensorsEqual(float *T1, float *T2, const unsigned Size, const float Epsilon=1e-6);
+bool tensorsEqual(float *T1, float *T2, const unsigned Size,
+                  const float Epsilon = 1e-6);
 
 void randomizeTensor(float *&Tensor, unsigned Size);
 
 void fillSerialTensor(float *&Tensor, unsigned Size);
 
-void printTensor(float *Tensor, std::vector<unsigned> Sizes, std::string Pre="", const std::string Post="\n", bool First=true);
+void printTensor(float *Tensor, std::vector<unsigned> Sizes,
+                 std::string Pre = "", const std::string Post = "\n",
+                 bool First = true, int Setw=-1);
 
-#endif // __UTILS_H_
+#endif // _UTILS_H_
