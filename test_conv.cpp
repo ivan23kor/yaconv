b1b247b758083f0ab6a69aa2009bcbf4f19f3a11
Ivan Korostelev <ivan23kor@gmail.com>
2021-11-18 13:49:24 -0600

    YaConv3.0: everything works
    TODOs:
    - set a few rows to 0 when pad > 0
    - correctly calculate extra space required

--- test_conv.cpp
+++ test_conv.cpp
@@ -22,12 +22,12 @@ void convertMCHWToMHWC(float *MCHW, float *MHWC, int M, int C, int H, int W) {
 }
 
 float *yaconvExtraOutput(int H, int W, int FH, int FW, int PW, int OW, int M) {
-  int WholeH = H % 6 == 0 ? H : H + 6 - H % 6;
+  int WholeH = H % 16 == 0 ? H : H + 16 - H % 16;
   int ExtraBefore = ((FH - 1) * OW + PW) * M;
   int OutputAndAfter = WholeH * OW * M + (W + PW - FW + 1) * M;
   // std::cout << ExtraBefore << " before and " << OutputAndAfter << " for output and after\n";
 
-  auto *Ret = alignedAlloc(ExtraBefore + OutputAndAfter);
+  auto *Ret = alignedAlloc(ExtraBefore + OutputAndAfter * 10);
   return Ret + ExtraBefore;
 }
 
@@ -54,13 +54,13 @@ int main(int argc, char **argv) {
   const int OW = (W - FW + 2 * PW) / SW + 1;
 
   // Create two input tensors - NCHW and NHWC
-  auto *InputCHW = allocateFilledTensor(C * H * W);
-  auto *InputHWC = allocateFilledTensor(H * W * C);
+  auto *InputCHW = allocateRandomTensor(C * H * W);
+  auto *InputHWC = allocateRandomTensor(H * W * C);
   convertCHWToHWC(InputCHW, InputHWC, C, H, W);
 
   // Create two filter tensors - MCHW and MHWC
-  auto *FilterMCHW = allocateFilledTensor(M * C * FH * FW);
-  auto *FilterMHWC = allocateFilledTensor(M * FH * FW * C);
+  auto *FilterMCHW = allocateRandomTensor(M * C * FH * FW);
+  auto *FilterMHWC = allocateRandomTensor(M * FH * FW * C);
   convertMCHWToMHWC(FilterMCHW, FilterMHWC, M, C, FH, FW);
 
   // Print Input and Filter tensors
@@ -91,18 +91,16 @@ int main(int argc, char **argv) {
   Times.push_back(TempTime / Repeat);
 
   // clang-format off
-  // Yaconv
-  Outputs.push_back(yaconvExtraOutput(H, W, FW, FW, PW, OW, M));
-  RUN(yaconv(InputHWC, FilterMHWC, Outputs.back(), C, H, W, M, FH, FW, PH, PW, SH, SW))
-  std::cout << YaConvPack1 / Repeat << "\n";
-  std::cout << YaConvPack2 / Repeat << "\n";
-  std::cout << YaConvComp / Repeat << "\n";
-
   // Convolution with im2col
   auto *im2colOutputMHW = allocateFilledTensor(M * OH * OW);
   RUN(convIm2col(InputCHW, FilterMCHW, im2colOutputMHW, C, H, W, M, FH, FW, OH, OW, PH, PW, SH, SW))
   Outputs.push_back(alignedAlloc(OH * OW * M));
-  convertCHWToHWC(im2colOutputMHW, Outputs[1], M, OH, OW);
+  convertCHWToHWC(im2colOutputMHW, Outputs.back(), M, OH, OW);
+
+  // Yaconv
+  Outputs.push_back(yaconvExtraOutput(H, W, FW, FW, PW, OW, M));
+  RUN(yaconv(InputHWC, FilterMHWC, Outputs.back(), C, H, W, M, FH, FW, PH, PW, SH, SW))
+  std::cout << "L3Pack: " << YaConvPack1 / Repeat << ", L2Pack: " << YaConvPack2 / Repeat << ", Comp: " << YaConvComp / Repeat << "\n";
   // clang-format on
 
   // Print tensors for each run
@@ -112,18 +110,18 @@ int main(int argc, char **argv) {
   // Print times for each run
   for (const auto &Time : Times)
     std::cout << Time << "\n";
-  for (const auto &Time : Times)
-    std::cout << 2 * M * C / 1000 * FH * FW * OH / 1000 * OW / Time / 1000 << "\n";
+  // for (const auto &Time : Times)
+  //   std::cout << 2 * M * C / 1000 * FH * FW * OH / 1000 * OW / Time / 1000 << "\n";
   // std::cout << Im2colTime / Repeat << "," << GEMMTime / Repeat << "\n";
 
   std::cout << "MaxDiff: " << maxTensorDiff(Outputs, M * OH * OW) << "\n";
 
   // Free tensor memory
-  free(InputCHW);
-  free(InputHWC);
-  free(FilterMCHW);
-  free(FilterMHWC);
-  free(im2colOutputMHW);
+  // free(InputCHW);
+  // free(InputHWC);
+  // free(FilterMCHW);
+  // free(FilterMHWC);
+  // free(im2colOutputMHW);
   // for (const auto &Output: Outputs)
   //   free(Output);
   return 0;
--- yaconv.cpp
+++ yaconv.cpp
@@ -4,6 +4,7 @@ namespace {
 };
 #include "utils.hpp"  // Tensor aligned allocation and printing
 #include <chrono>     // Timing
+#include <cmath>      // std::ceil
 #include <iostream>   // Debug printing
 
 // Timing
@@ -14,35 +15,16 @@ double YaConvPack1 = 0.0, YaConvPack2 = 0.0, YaConvComp = 0.0;
 // TODO: Convolution parameters as a struct
 static int C, H, W, M, FH, FW, OH, OW, SH, SW, PH, PW;
 
-inline bool yaconvCanHandle() {
-  return !((SH != 1)
-      // Haven't figured out how to slice bigger matrices yet
-      || (C > BLOCK_KC)
-      // Image must fit in TLB/L2
-      || (std::ceil((float)H / BLOCK_MR) * BLOCK_MR * W * C > BLOCK_MC * BLOCK_KC)
-      // Filter must fit in L3
-      || (std::ceil((float)M / BLOCK_NR) * BLOCK_NR * FH * FW * C > BLOCK_KC * BLOCK_NC));
+// Image is H x W x C
+void packImage(float *Image, float *Pack, int NC) {
+  for (int nr = 0; nr < NC; nr += BLOCK_NR)
+    packBPanel(Image + nr * W * C, Pack + nr * W * C, MIN(NC - nr, BLOCK_NR), W * C, W * C, 1, BLOCK_NR);
 }
 
-// Assumes Image is H x W x C
-void packImage(float *Image, float *Pack) {
-  int mr = 0;
-  for(; mr + BLOCK_MR <= H; mr += BLOCK_MR)
-    packAPanel(Image + mr * W * C, Pack + mr * W * C, BLOCK_MR, W * C, W * C, 1, BLOCK_MR);
-  packAPanel(Image + mr * W * C, Pack + mr * W * C, H - mr, W * C, W * C, 1, BLOCK_MR);
-}
-
-// Assumes Filter is M x FH x FW x C
-void packFilter(float *Filter, float *Pack) {
-  int WholeM = M % BLOCK_NR == 0 ? M : M + BLOCK_NR - M % BLOCK_NR;
-
-  int K = FH * FW * C;
-  for (int f = 0; f < K; f += C) {
-    int nr = 0;
-    for (; nr + BLOCK_NR <= M; nr += BLOCK_NR)
-      packBPanel(Filter + f + nr * K, Pack + f * WholeM + nr * C, BLOCK_NR, C, K, 1, BLOCK_NR);
-    packBPanel(Filter + f + nr * K, Pack + f * WholeM + nr * C, M - nr, C, K, 1, BLOCK_NR);
-  }
+// Filter is M x FH x FW x C
+void packFilter(float *Filter, float *Pack, int MC, int KC) {
+  for (int mr = 0; mr < MC; mr += BLOCK_MR)
+    packAPanel(Filter + mr * FH * FW * C, Pack + mr * KC, MIN(MC - mr, BLOCK_MR), KC, FH * FW * C, 1, BLOCK_MR);
 }
 
 void yaconv(float *Image, float *Filter, float *Output,
@@ -54,72 +36,100 @@ void yaconv(float *Image, float *Filter, float *Output,
   OH = (H - FH + 2 * PH) / SH + 1;
   OW = (W - FW + 2 * PW) / SW + 1;
 
-  // Only unit strides, for other strides im2col has less overhead anyway
-  if (!yaconvCanHandle()) {
-    std::cout << "\033[31myaconv con't handle such convolution yet\033[0m\n";
-    exit(-1);
-  }
-
-  // Initialize all Output elements to zero
-  bli_sset0s_mxn(M, OH * OW, Output, OH * OW, 1);
-
-  // Pack Filter in L3
-  t1 = high_resolution_clock::now();
-  auto *FilterPack = alignedAlloc(BLOCK_KC * BLOCK_NC);
-  packFilter(Filter, FilterPack);
-  t2 = high_resolution_clock::now();
-  YaConvPack1 += duration_cast<duration<double>>(t2 - t1).count();
-  // printTensor(Filter, {M, FH* FW* C});
-  // printTensor(FilterPack, {FH * FW * C * (int)std::ceil((float)M / BLOCK_NR), BLOCK_NR});
-
-  // Pack Image in L2
-  t1 = high_resolution_clock::now();
-  auto *ImagePack = alignedAlloc(BLOCK_MC * BLOCK_KC);
-  packImage(Image, ImagePack);
-  t2 = high_resolution_clock::now();
-  YaConvPack2 += duration_cast<duration<double>>(t2 - t1).count();
+  // Pack Image in L3
+  auto *ImagePack = alignedAlloc(BLOCK_MC * BLOCK_NC);
   // printTensor(Image, {H, W * C});
-  // printTensor(ImagePack, {W * C * (int)std::ceil((float)H / BLOCK_MR), BLOCK_MR});
+  // packImage(Image, ImagePack);
+  // printTensor(ImagePack, {W * C * (int)std::ceil((float)H / BLOCK_NR), BLOCK_NR});
 
-  t1 = high_resolution_clock::now();
-  int WholeM = M % BLOCK_NR == 0 ? M : M + (BLOCK_NR - M % BLOCK_NR);
-  int WholeH = H % BLOCK_MR == 0 ? H : H + (BLOCK_MR - H % BLOCK_MR);
-  int MinOff = 0, MaxOff = 0;
-  Output += (PH * OW + PW) * M;
-  for (int f = 0; f < FH * FW; ++f) {
-    int fh = f / FW, fw = f % FW;
-    int FromRow = MAX(0, fh - PH);
-    int UntilRow =  MIN(H, fh + H + PH - FH + 1);
-    int FromCol = MAX(0, fw - PW);
-    int UntilCol =  MIN(W, fw + W + PW - FW + 1);
-    for (int m = 0; m < M; m += BLOCK_NR) {
-      for (int w = FromCol; w < UntilCol; ++w) {
-        for (int h = 0; h < H; h += BLOCK_MR) {
-          float *Ar = ImagePack + w * BLOCK_MR * C + h * W * C;
-          float *Br = FilterPack + f * WholeM * C + m * C;
-          // printTensor(Ar, {C, BLOCK_MR});
-          // printTensor(Br, {C, BLOCK_NR});
-          int off = ((h - fh) * OW + w - fw) * M + m;
-          // std::cout << f << "(f), " << m << "(m), " << w << "(w), " << h << "(h), " << off << "(off) " << FromRow << " -> " << UntilRow << "\n";
-          MinOff = MIN(MinOff, off);
-          MaxOff = MAX(MaxOff, off);
-          float *Cr = Output + off;
-          sgemm_ukr(C, bli_s1, Ar, Br, bli_s1, Cr, OW * M, 1);
-          // printTensor(Output, {OH * OW, M});
+  // Pack Filter in L2
+  auto *FilterPack = alignedAlloc(BLOCK_MC * BLOCK_KC);
+  // printTensor(Filter, {M, FH * FW * C});
+  // int MC = M, KC = 10;
+  // packFilter(Filter, FilterPack, MC, KC);
+  // printTensor(FilterPack, {KC * (int)std::ceil((float)M / BLOCK_MR), BLOCK_MR});
+
+  auto *CBuff = alignedAlloc(BLOCK_MR * BLOCK_NR);
+
+  // TODO: Add an extra loop over H; this assumes Image fits in L3, which is almost always true
+  int Debug = 1;
+  if (Debug == 2)
+    t1 = high_resolution_clock::now();
+  packImage(Image, ImagePack, H);
+  if (Debug == 1) {
+    printTensor(Image, {H, W * C});
+    printTensor(Filter, {M, FH * FW * C});
+    printTensor(ImagePack, {W * C * (int)std::ceil((float)H / BLOCK_NR), BLOCK_NR});
+  }
+  if (Debug == 2) {
+    t2 = high_resolution_clock::now();
+    YaConvPack1 += duration_cast<duration<double>>(t2 - t1).count();
+    t3 = high_resolution_clock::now();
+  }
+  for (int m = 0; m < M; m += BLOCK_MC) {
+    int MC = MIN(M - m, BLOCK_MC);
+    for (int fh = 0; fh < FH; ++fh) {
+      for (int kc = 0; kc < FW * C; kc += BLOCK_KC) {
+        float *Beta = (fh == 0) && (kc == 0) ? bli_s0 : bli_s1;
+        int KC = MIN(FW * C - kc, BLOCK_KC);
+        if (Debug == 2)
+          t1 = high_resolution_clock::now();
+        packFilter(Filter + (m * FH + fh) * FW * C + kc, FilterPack, MC, KC);
+        if (Debug == 1)
+          printTensor(FilterPack, {KC * (int)std::ceil((float)MC / BLOCK_MR), BLOCK_MR});
+        if (Debug == 2) {
+          t2 = high_resolution_clock::now();
+          YaConvPack2 += duration_cast<duration<double>>(t2 - t1).count();
+        }
+        for (int nr = 0; nr < H; nr += BLOCK_NR) {
+          for (int ow = 0; ow < OW; ++ow) {
+            int ImageStart = (ow - PW) * C + kc;
+            int ImageEnd = MIN(W * C, ImageStart + KC);
+
+            float *Ar = FilterPack;
+            if (ImageStart < 0) {
+              Ar -= ImageStart * BLOCK_MR;
+              ImageStart = 0;
+            }
+
+            int K = ImageEnd - ImageStart;
+            if (Debug == 1)
+              std::cout << "fh = " << fh << ", ImageStart = " << ImageStart << ", K = " << K << ", OutputOff = " << ((nr - fh + PH) * OW + ow) * M + m<< "\n";
+            if (K <= 0)
+              continue;
+
+            float *Br = ImagePack + nr * W * C + ImageStart * BLOCK_NR;
+            float *Cr = Output + ((nr - fh + PH) * OW + ow) * M + m;
+
+            for (int mr = 0; mr < MC; mr += BLOCK_MR) {
+              if (Debug == 1) {
+                std::cout << "Going to gemm Weight:\n";
+                printTensor(Ar, {K, BLOCK_MR});
+                std::cout << "and Image" << std::endl;
+                printTensor(Br, {K, BLOCK_NR});
+              }
+              if (mr + BLOCK_MR <= MC) {
+                bli_sset0s_mxn(1, BLOCK_MR, Cr, 1, OW * M);
+                sgemm_ukr(K, bli_s1, Ar, Br, Beta, Cr, 1, OW * M);
+              } else {
+                sgemm_ukr(K, bli_s1, Ar, Br, bli_s0, CBuff, BLOCK_NR, 1);
+                bli_sxpbys_mxn(MC - mr, BLOCK_NR, CBuff, BLOCK_NR, 1, Beta, Cr, 1, OW * M);
+              }
+              if (Debug == 1) {
+                std::cout << "so now the result is:" << std::endl;
+                printTensor(Output, {OH * OW, M});
+              }
+
+              Ar += BLOCK_MR * KC;
+              Cr += BLOCK_MR;
+            }
+          }
         }
       }
     }
   }
-  t2 = high_resolution_clock::now();
-  YaConvComp += duration_cast<duration<double>>(t2 - t1).count();
-  return;
-  int MyMinOff = 0 - ((FH - 1) * OW + PW) * M;
-  if (MinOff != MyMinOff)
-    std::cout << "Min: " << MinOff  << " != " << MyMinOff << "\n";
-  // MaxOff contains the offset of the beginning of the farthest row.
-  // After this, MaxOff will contain the beginning of the next row
-  MaxOff += BLOCK_MR * OW * M + BLOCK_NR;
-  int MyMaxOff = WholeH * OW * M + (W + PW - FW + 1) * M;
-  if (MaxOff != MyMaxOff)
-    std::cout << "Max: " << MaxOff  << " != " << MyMaxOff << "\n";
+  if (Debug == 2) {
+    t4 = high_resolution_clock::now();
+    YaConvComp += duration_cast<duration<double>>(t4 - t3).count();
+  }
 }
